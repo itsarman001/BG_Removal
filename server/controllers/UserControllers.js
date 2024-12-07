2893c679a4e35bdd597fba8b9c8ba8d22557fa5c
arman <armanar6712@gmai.com>
2024-12-07 23:54:40 +0530

    feat: add auth middleware and user credits route

--- server/controllers/UserControllers.js
+++ server/controllers/UserControllers.js
@@ -59,4 +59,15 @@ const clerkWebHook = async (req, res) => {
   }
 };
 
-export { clerkWebHook };
+const userCredits = async (req, res) => {
+  try {
+    const { clerkId } = req.body;
+    const userData = await userModel.findOne({ clerkId });
+    res.json({ success: true, credits: userData.creditBalance });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { clerkWebHook, userCredits };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
-import express from 'express'
-import { clerkWebHook } from '../controllers/UserControllers.js'
+import express from 'express';
+import { clerkWebHook, userCredits } from '../controllers/UserControllers.js';
+import authUser from '../middleware/auth.js';
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 userRouter.post('/webhooks', clerkWebHook);
+userRouter.get('/credits', authUser, userCredits);
 
 export default userRouter;
